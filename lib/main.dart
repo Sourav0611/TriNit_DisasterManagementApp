aa23af66d7b212eea5252cf773f88ba13d6d60b8
AnkitGourav <Ankitgourav035@gmail.com>
2024-03-09 17:55:26 +0530

    Add Sign-in page.
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:trinitw/resources/values/strings.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,8 +14,7 @@ void main() async {
         apiKey: "AIzaSyDR2-RN98Zr6NN9lH8gb8jD6DTrm45iQcc",
         appId: "1:312371631478:android:49f6f02015e8145665d657",
         messagingSenderId: "312371631478",
-        projectId: "tri-nitw"
-    ),
+        projectId: "tri-nitw"),
   );
   runApp(const MyApp());
 }
@@ -36,7 +41,123 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.health_and_safety_rounded,
+                color: Theme.of(context).colorScheme.primary,
+                size: 128.0,
+              ),
+              const SizedBox(height: 24.0),
+              Text(
+                Strings.signInPageTitle,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                Strings.signInPageSubTitle,
+                style: TextStyle(fontSize: 16.0),
+              ),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                    ),
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: Strings.userNameHint,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                    ),
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: Strings.passwordHint,
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Container(
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: Center(
+                    child: Text(
+                      Strings.signIn,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    Strings.notAMember,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(width: 5.0),
+                  Text(
+                    Strings.registerNow,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
---
+++ lib/resources/values/strings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Strings {
+  static const String appName = "Disaster Management App";
+
+  static const String signInPageTitle = "Safety";
+  static const String signInPageSubTitle = "At your finger tips.";
+
+  static const String userNameHint = "Username";
+  static const String passwordHint = "Password";
+  static const String signIn = "Sign-in";
+  static const String notAMember = "Not a member?";
+  static const String registerNow = "Register now";
+}
