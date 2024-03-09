891e223a06f773dcc9bea3189fd0f7e9cf7b148f
AnkitGourav <Ankitgourav035@gmail.com>
2024-03-09 17:01:17 +0530

    Init empty flutter project with firebase.
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

---
+++ android/app/src/main/kotlin/dev/bytebuddies/trinitw/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.bytebuddies.trinitw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyDR2-RN98Zr6NN9lH8gb8jD6DTrm45iQcc",
+        appId: "1:312371631478:android:49f6f02015e8145665d657",
+        messagingSenderId: "312371631478",
+        projectId: "tri-nitw"
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Disaster Management App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
